be4df30193c4271a780db2888dfd4b8aca99ee9a
Nhat Thien <thienvo0211@gmail.com>
2020-01-16 16:21:53 +0700

    add sync submission

--- submission/src/main/java/com/fpt/submission/utils/UploadFile.java
+++ submission/src/main/java/com/fpt/submission/utils/UploadFile.java
@@ -2,7 +2,12 @@ package com.fpt.submission.utils;
 
 import com.fpt.submission.dto.request.UploadFileDto;
 import com.fpt.submission.exception.CustomException;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.http.HttpStatus;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.util.ResourceUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,19 +18,32 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+@EnableAsync
 public class UploadFile {
+    private static final String JAVA_CONSOLE_FOLDER_PATH = "\\practical\\JavaConsole";
 
-  public static void uploadFile( UploadFileDto dto) {
-      try {
-          MultipartFile file = dto.getFile();
-          if(file != null) {
-              String folPath = ResourceUtils.getFile("classpath:static").getAbsolutePath();
-              Path copyLocation = Paths.get(folPath + File.separator + StringUtils.cleanPath(file.getOriginalFilename()));
-              Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
-          }
-      } catch (Exception ex) {
-          System.out.println(ex.getMessage());
-          throw new CustomException(HttpStatus.CONFLICT,ex.getMessage());
-      }
-  }
+    @Bean("ThreadPoolTaskExecutor")
+    public TaskExecutor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(20);
+        executor.setMaxPoolSize(1000);
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setThreadNamePrefix("Async-");
+        return executor;
+    }
+
+    @Async("ThreadPoolTaskExecutor")
+    public static void uploadFile(UploadFileDto dto) {
+        try {
+            MultipartFile file = dto.getFile();
+            if (file != null) {
+                String folPath = ResourceUtils.getFile("classpath:static" + JAVA_CONSOLE_FOLDER_PATH).getAbsolutePath();
+                Path copyLocation = Paths.get(folPath + File.separator + StringUtils.cleanPath(file.getOriginalFilename()));
+                Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            throw new CustomException(HttpStatus.CONFLICT, ex.getMessage());
+        }
+    }
 }
